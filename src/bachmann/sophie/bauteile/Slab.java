e966d889e485242326182e1cb3f2064c3e7327b9
Sophie Bachmann <Sophie@Fines-MbP.fritz.box>
2019-12-29 14:40:49 +0100

    Slab Klasse-Variablen deklariert

--- src/bachmann/sophie/bauteile/Slab.java
+++ src/bachmann/sophie/bauteile/Slab.java
@@ -1,5 +1,8 @@
 package bachmann.sophie.bauteile;
 
 public class Slab extends Stahlbetonbauteil {
-
+	//private variables
+	private double width;
+	private double length;
+	private double height;
 }
